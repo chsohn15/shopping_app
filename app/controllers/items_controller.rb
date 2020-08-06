6dc633c86f0408bf3e1f3ff8ff9946a8e0dbaae5
chsohn15 <chsohn15@gmail.com>
2020-08-06 14:31:41 -0400

    Added edit and update features

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,30 @@ class ItemsController < ApplicationController
         id = params[:id]
         @item = Item.find(id)
     end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        item = Item.create(item_params)
+        redirect_to item_path(item)
+    end
+
+    def edit
+        @item = Item.find(params[:id])
+
+    end
+
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        redirect_to item_path(@item)
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:name, :price, :brand, :color, :size, :img_url)
+    end
 end
